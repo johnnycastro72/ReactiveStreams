c37fb849c5fc94bf580897e1cc8df92a56952357
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-25 14:28:10 -0500

    Third Exercise
    Third exercise solved

--- src/main/java/com/example/functional/FunctionalApplication.java
+++ src/main/java/com/example/functional/FunctionalApplication.java
@@ -8,18 +8,29 @@ import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @SpringBootApplication
 public class FunctionalApplication {
 
+	public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor)
+	{
+		Map<Object, Boolean> map = new ConcurrentHashMap<>();
+		return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+	}
+
 
 	public static void main(String[] args) {
-		//I stablish the connection
+		//I establish the connection
 		MongoClient mongoClient = MongoClients.create("mongodb+srv://mdyagual:mdyagual@clusterferreteria.aum6z.mongodb.net/?retryWrites=true&w=majority");
 		// I'm getting the table/collection that i want to use
 		MongoCollection<Document> collection = mongoClient.getDatabase("sample_restaurants").getCollection("restaurants");
@@ -27,17 +38,17 @@ public class FunctionalApplication {
 		ArrayList<Document> dataRestaurants = collection.find().into(new ArrayList<>());
 
 		System.out.println("\n\n------OUTPUT------------\n\n");
-		/* Use this line to remember how the data looks like
-		dataRestaurants.stream().map(Document::toJson).limit(5).forEach(System.out::println);*/
+		// Use this line to remember how the data looks like
+		dataRestaurants.stream().map(Document::toJson).limit(5).forEach(System.out::println);
 
 		//1: Get the boroughs that start with letter 'B'
 
 		//First we define the condition to use on filter
 		Predicate<Document> boroughWithB = (restaurant -> restaurant.get("borough").toString().startsWith("B"));
-		//Second we define a function that will received the 'database' and will return a Stream of documents
+		//Second we define a function that will receive the 'database' and will return a Stream of documents
 		Function<ArrayList<Document>, Stream<Document>> getBoroughs = (dbRest) -> dbRest.stream()
 				.filter(r -> boroughWithB.test(r));
-		//Finally show the result
+		//Finally, show the result
 		Consumer<Stream<Document>> resultF1 = (value) -> value.forEach(r -> System.out.println(r.get("name")+": "+r.get("borough")));
 		System.out.println("Filter #1");
 
@@ -48,7 +59,7 @@ public class FunctionalApplication {
 
 		//First we define the condition to use on filter
 		Predicate<Document> isAmerica = (data) -> data.get("cuisine").toString().equals("American");
-		//Second we define a function that will received the 'database' and will return a Stream of documents
+		//Second we define a function that will receive the 'database' and will return a Stream of documents
 		Function<ArrayList<Document>,Stream<Document>> cuisineAmerican = (dbRest) -> dbRest.stream()
 				.filter(r -> isAmerica.test(r));
 		//Finally show the result
@@ -58,13 +69,31 @@ public class FunctionalApplication {
 
 		//TO DO
 		/*3: Get the amount of restaurants whose name is just one word
-		Keep it in mind that a restaurant e.g McDonals have some locals in different directions and also some records hasn't names assigned
-		HINT: Remember that if the restaurant's name has spaces that means it has more than 1 word*/
+		Keep it in mind that a restaurant e.g. McDonald have some locals in different directions and also some records hasn't names assigned
+		HINT: Remember that if the restaurant's name has spaces that mean it has more than 1 word*/
+
+		//First we define the condition to use on filter
+		Predicate<Document> oneWord = (data) -> (data.get("name").toString().split(" ").length == 1)
+				&& (data.get("name").toString() != null)
+				&& (!data.get("name").toString().isBlank());
+
+		//Second we define a function that will receive the 'database' and will return a Stream of documents
+		Function<ArrayList<Document>, Long> oneWordNamed = (dbRest) -> dbRest.stream()
+				.filter(r -> oneWord.test(r))
+				.filter(distinctByKey(p -> p.get("name")))
+				.count();
+
+		//Finally, show the result
+
+		System.out.println("Filter #3");
+		System.out.println("The number of restaurants whose name has only one word is: " + oneWordNamed.apply(dataRestaurants));
 
 		/*4: Get all the restaurants that received grade C in the most recent data
 		HINT: The recent score is always the first one inside the list of the key "grades".*/
 
-		/*5: Sort all the restaurants by the grade that has received in the most recent date. If the are not receiving a grade yet (grade=Not Yet Graded), ignore them.
+
+
+		/*5: Sort all the restaurants by the grade that has received in the most recent date. If they are not receiving a grade yet (grade=Not Yet Graded), ignore them.
 		* HINT: Consider create a small Restaurant object with the data that you need to archive this exercise*/
 
 
@@ -72,14 +101,14 @@ public class FunctionalApplication {
 
 		/*7 (Optional): Investigate zip function (import org.springframework.data.util.StreamUtils;) to generate a list of strings with the next elements:
 		-A stream that contains all the names of the restaurant
-		-Another stream that contains the amount of words used on the restaurants's name
+		-Another stream that contains the amount of words used on the restaurant's name
 		Output expected
 		xxxxx has 1 word
 		xxxx yyyyy zz have 3 words
 		xxxx yyyy have 2 words
 		.
 		.
-		If you can solved it and if in the calculator activity you don't complete/implement the division operation, this will get considerer.
+		If you can solve it and if in the calculator activity you don't complete/implement the division operation, this will get considered.
 		*/
 
 
